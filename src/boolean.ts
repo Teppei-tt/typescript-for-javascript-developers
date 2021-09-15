7b5bfe6f174c84ece980396e2a4014a9e6c59e39
Teppei-tt <teppei.takata03@gmail.com>
2021-09-15 20:38:38 +0900

    fix ts-node-dev issue by roalbackking ts-node-version

---
+++ src/boolean.ts
@@ -0,0 +1,8 @@
+export {};
+
+let name =  'TypeScript';
+//(: boolean → 型注釈、型アノテーション、アノテーション)という。
+let isFinished: boolean = true;
+isFinished = false;
+isFinished = 1;
+console.log({ isFinished });
