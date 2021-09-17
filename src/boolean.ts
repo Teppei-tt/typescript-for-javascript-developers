9621121f84390420636c9a7537bc4c2f7fdf453c
Teppei-tt <teppei.takata03@gmail.com>
2021-09-17 20:51:36 +0900

    コメントアウト

--- src/boolean.ts
+++ src/boolean.ts
@@ -4,5 +4,5 @@ let name =  'TypeScript';
 //(: boolean → 型注釈、型アノテーション、アノテーション)という。
 let isFinished: boolean = true;
 isFinished = false;
-isFinished = 1;
+// isFinished = 1;
 console.log({ isFinished });
