660b6b4f4e08e103ba919bb6af490659a136ce07
Teppei-tt <teppei.takata03@gmail.com>
2021-09-18 20:23:17 +0900

    ✨ 配列の型

---
+++ src/array.ts
@@ -0,0 +1,15 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+
+// 非推奨の書き方
+let numbers2: Array<number> = [1, 2, 3];
+
+let strings: string[] = ["TypeScript", "JavaScript"];
+
+let nizigenhairetu: number[][] = [
+  [50, 100],
+  [100, 200],
+];
+
+let hairetu: (string | number | boolean)[] = [1, false, "japan"];
